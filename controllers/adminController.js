d9da2cb1bc57a0fd931ef74594b9e9ce298d02a1
Atharvan <atharvanpohnerkar2@gmail.com>
2023-11-17 19:50:07 +0530

    feat: image upload

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,9 +5,17 @@ import { Order } from "../models/Orders.js";
 import { Multimotor, Airplane, Fpv, Propeller, Esc } from "../models/Mutimotors.js"
 import { mailSend } from "../utils/sendEmail.js";
 import { model } from "mongoose";
+import imgur from "imgur";
+import fileUpload from "express-fileupload";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
 const models = [Multimotor, Airplane, Fpv, Propeller, Esc];
-const models_obj = {Multimotor, Airplane, Fpv, Propeller, Esc};
+const models_obj = { Multimotor, Airplane, Fpv, Propeller, Esc };
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const getAllUsers = asyncErrorHandler(async (req, res, next) => {
     if (req.isAdmin) {
@@ -99,6 +107,49 @@ const updateOrder = asyncErrorHandler(async (req, res, next) => {
 const createProduct = asyncErrorHandler(async (req, res, next) => {
 
     if (req.isAdmin) {
+        const imageArray = [];
+        const { fileInput } = req.files;
+
+        if (!fileInput || (Array.isArray(fileInput) && fileInput.length === 0)) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        const files = Array.isArray(fileInput) ? fileInput : [fileInput];
+
+        try {
+            const uploadPromises = files.map(async (file) => {
+                const uploadFilePath = path.join(uploadPath, file.name);
+
+                // Move the uploaded file to the specified path
+                await file.mv(uploadFilePath);
+
+                // Upload file to imgur
+                return imgur.uploadFile(uploadFilePath)
+                    .then((json) => {
+                        console.log(json.data.link);
+                        imageArray.push(json.data.link);
+
+                        // If the upload is successful, delete the local file
+                        return fs.unlink(uploadFilePath);
+                    })
+                    .catch((err) => {
+                        console.error('Error during imgur upload:', err);
+
+                        // Handle the error appropriately
+                        throw err;
+                    });
+            });
+
+            // Wait for all uploads and deletions to complete
+            await Promise.all(uploadPromises);
+
+            // If all files are uploaded successfully and deleted, you can send a success message
+            res.send('Files uploaded and deleted successfully!');
+        } catch (err) {
+            console.error('Error during file upload:', err);
+            res.status(500).send(err.message || 'Internal Server Error');
+        }
+
         const product = {
             type: req.body.type,
             name: req.body.name,
@@ -108,8 +159,9 @@ const createProduct = asyncErrorHandler(async (req, res, next) => {
             weight: req.body.weight,
             min_quantity: req.body.min_quantity,
             category: req.body.category,
-            image_url: req.body.image_url
+            image_url: imageArray
         }
+
         const selectedModel = models_obj[req.body.type]
         await selectedModel.insertMany(product).then((err) => {
             res.status(200).json({
@@ -152,48 +204,91 @@ const updateProduct = asyncErrorHandler(async (req, res, next) => {
     if (req.isAdmin) {
         const productId = req.params.id;
         const existingProduct = await findProductById(productId);
+        const imageArray = [];
+        const { fileInput } = req.files;
 
+        if (!fileInput || (Array.isArray(fileInput) && fileInput.length === 0)) {
 
-        if (req.body.type) {
-            existingProduct.type = req.body.type;
         }
-        if (req.body.name) {
-            existingProduct.name = req.body.name;
-        }
-        if (req.body.description) {
-            existingProduct.description = req.body.description;
-        }
-        if (req.body.price) {
-            existingProduct.price = req.body.price;
-        }
-        if (req.body.kv) {
-            existingProduct.kv = req.body.kv;
-        }
-        if (req.body.weight) {
-            existingProduct.weight = req.body.weight;
-        }
-        if (req.body.min_quantity) {
-            existingProduct.min_quantity = req.body.min_quantity;
-        }
-        if (req.body.category) {
-            existingProduct.category = req.body.category;
-        }
-        if (req.body.image_url) {
-            existingProduct.image_url = req.body.image_url;
-        }
+        else {
+            const files = Array.isArray(fileInput) ? fileInput : [fileInput];
 
-        await existingProduct.save().catch((err) => {
-            res.status(400).json("Error in updating " + err);
-        });
+            try {
+                const uploadPromises = files.map(async (file) => {
+                    const uploadFilePath = path.join(uploadPath, file.name);
 
-        res.status(200).json({
-            success: true,
-            existingProduct
-        })
-    } else {
+                    // Move the uploaded file to the specified path
+                    await file.mv(uploadFilePath);
+
+                    // Upload file to imgur
+                    return imgur.uploadFile(uploadFilePath)
+                        .then((json) => {
+                            console.log(json.data.link);
+                            imageArray.push(json.data.link);
+
+                            // If the upload is successful, delete the local file
+                            return fs.unlink(uploadFilePath);
+                        })
+                        .catch((err) => {
+                            console.error('Error during imgur upload:', err);
+
+                            // Handle the error appropriately
+                            throw err;
+                        });
+                });
+
+                // Wait for all uploads and deletions to complete
+                await Promise.all(uploadPromises);
+
+                // If all files are uploaded successfully and deleted, you can send a success message
+                res.send('Files uploaded and deleted successfully!');
+            }
+
+            catch (err) {
+                console.error('Error during file upload:', err);
+                res.status(500).send(err.message || 'Internal Server Error');
+            }
+
+            if (req.body.type) {
+                existingProduct.type = req.body.type;
+            }
+            if (req.body.name) {
+                existingProduct.name = req.body.name;
+            }
+            if (req.body.description) {
+                existingProduct.description = req.body.description;
+            }
+            if (req.body.price) {
+                existingProduct.price = req.body.price;
+            }
+            if (req.body.kv) {
+                existingProduct.kv = req.body.kv;
+            }
+            if (req.body.weight) {
+                existingProduct.weight = req.body.weight;
+            }
+            if (req.body.min_quantity) {
+                existingProduct.min_quantity = req.body.min_quantity;
+            }
+            if (req.body.category) {
+                existingProduct.category = req.body.category;
+            }
+            if (imageArray.length != 0) {
+                existingProduct.image_url = imageArray;
+            }
+
+            await existingProduct.save().catch((err) => {
+                res.status(400).json("Error in updating " + err);
+            });
+
+            res.status(200).json({
+                success: true,
+                existingProduct
+            })
+        } 
+
+    }else {
         res.status(400).json("Not an admin")
-    }
-
-});
+    }});
 
 export { getAllOrders, getAllUsers, getOrder, updateOrder, createProduct, deleteProduct, updateProduct, getAllProducts }
