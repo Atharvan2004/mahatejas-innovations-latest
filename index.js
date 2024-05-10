74f837d056e4c08b56287c67e4a512c02fc3da92
Atharvan <atharvanpohnerkar2@gmail.com>
2024-05-10 10:55:53 +0530

    cors 2

--- index.js
+++ index.js
@@ -22,14 +22,16 @@ await conn();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mahatejas-innovations-latest-f-atharvans-projects.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-app.use(cors());
+const allowedOrigins = [
+  'https://mahatejas-innovations-latest-f-atharvans-projects.vercel.app',
+  'https://mahatejas-innovations-latest-f-git-main-atharvans-projects.vercel.app',
+  'mahatejas-innovations-latest-f.vercel.app',
+];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
